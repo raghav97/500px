d603db312f56f8b8ef960d9729fa3c445077034e
raghav_vc <vc.raghav@gmaill.com>
2017-02-18 20:09:15 +0530

    Added view partial and controller methods

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,21 +1,87 @@
 class StaticController < ApplicationController
   def homepage
-    
+    @response = F00px.get('photos?feature=popular') 
+    @a = @response.body
+    @images = []
+    @title = []
+    @first_name = []
+    @last_name = []
+
+    @hash = JSON.parse(@a)
+    @hash["photos"].each do |e|
+      @images.push(e["image_url"])
+      @title.push(e["name"])
+      @first_name.push(e["user"]["firstname"])
+      @last_name.push(e["user"]["lastname"])
+    end
   end
 
   def fresh_yesterday
-
+    @response = F00px.get('photos?feature=fresh_yesterday') 
+    @a = @response.body
+    @images = []
+    @title = []
+    @first_name = []
+    @last_name = []
+    
+    @hash = JSON.parse(@a)
+    @hash["photos"].each do |e|
+      @images.push(e["image_url"])
+      @title.push(e["name"])
+      @first_name.push(e["user"]["firstname"])
+      @last_name.push(e["user"]["lastname"])
+    end
   end
 
   def fresh_week
-
+    @response = F00px.get('photos?feature=fresh_week') 
+    @a = @response.body
+    @images = []
+    @title = []
+    @first_name = []
+    @last_name = []
+    
+    @hash = JSON.parse(@a)
+    @hash["photos"].each do |e|
+      @images.push(e["image_url"])
+      @title.push(e["name"])
+      @first_name.push(e["user"]["firstname"])
+      @last_name.push(e["user"]["lastname"])
+    end
   end
 
   def fresh_today
-
+    @response = F00px.get('photos?feature=fresh_today') 
+    @a = @response.body
+    @images = []
+    @title = []
+    @first_name = []
+    @last_name = []
+    
+    @hash = JSON.parse(@a)
+    @hash["photos"].each do |e|
+      @images.push(e["image_url"])
+      @title.push(e["name"])
+      @first_name.push(e["user"]["firstname"])
+      @last_name.push(e["user"]["lastname"])
+    end
   end
 
   def editors
-
+    @response = F00px.get('photos?feature=editors') 
+    @a = @response.body
+    @images = []
+    @title = []
+    @first_name = []
+    @last_name = []
+    
+    @hash = JSON.parse(@a)
+    @hash["photos"].each do |e|
+      @images.push(e["image_url"])
+      @title.push(e["name"])
+      @first_name.push(e["user"]["firstname"])
+      @last_name.push(e["user"]["lastname"])
+    end
   end
+
 end 
